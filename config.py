e94e9e2f54aad781ff78cfa32d5313d407f8c064
chinancheng <gfjmgj1456@gmail.com>
2018-04-03 10:15:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ class Config(object):
     lr = 1e-4
     discount_factor = 0.99
     reply_buffer_size = 50000
-    total_episode = 300000
+    total_episode = 2000000
     update_target_frequency = 5
     initial_epsilon = 0.1
     min_epsilon = 0.0001
-    epsilon_discount_rate = 1e-5
+    epsilon_discount_rate = 1e-7
     save_video_frequency = 500
     save_logs_frequency = 500
     show_loss_frequency = 10
